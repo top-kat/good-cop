4cd723220effc13cea1fdcae050a52edde45c399
Luke Costelloe <lukostelloe@gmail.com>
2024-11-13 11:47:17 +0100

    date, date8

---
+++ tests/date8.test.ts
@@ -0,0 +1,31 @@
+
+
+
+import { _ } from '../src/DefinitionClass'
+
+
+
+describe(`Date8 definition`, () => {
+
+    const validDateString = '20231113'
+    // const invalidDateString = '20231332'
+
+    const date8Def = _.n('myDate').date8()
+
+    it('checks the return types of read or write as any', () => {
+        expect(date8Def.getTsTypeAsString()).toEqual({ 'read': 'number', 'write': 'number' })
+    })
+
+    it('validates and formats a valid 8-digit date string', async () => {
+        await expect(date8Def.formatAndValidate(validDateString)).resolves.toEqual(20231113);
+    });
+
+    it('validates and formats a valid 8-digit date number', async () => {
+        await expect(date8Def.formatAndValidate(20231113)).resolves.toEqual(20231113);
+    });
+
+    //ATT: this is still resolving, need to fix logic for INVALID DATE
+    // it('throws an error for an invalid 8-digit date string', async () => {
+    //     await expect(date8Def.formatAndValidate(invalidDateString)).rejects.toThrow('Expected type \'date8\' but got "20231332"');
+    // }); 
+})
