69ee284a3a66b612593ee435bbae30b4f1bab9e3
Luke Costelloe <lukostelloe@gmail.com>
2024-11-18 15:52:05 +0100

    unique

--- tests/second-level/unique.test.ts
+++ tests/second-level/unique.test.ts
@@ -1,25 +1,34 @@
 
 
 
-import { _ } from '../../src/DefinitionClass'
+import { _ } from '../../src/DefinitionClass';
+
+
 
 describe('Unique', () => {
 
-  const uniqueDef = _.object({ firstName: _.string().unique(), lastName: _.string() });
+    const uniqueDef = _.object({ firstName: _.string().unique(), lastName: _.string() });
 
-  it('checks the return types of read or write as a string', () => {
-    expect(uniqueDef.getTsTypeAsString()).toEqual({
-      read: `{\n    'firstName'?: string\n    'lastName'?: string\n}`,
-      write: `{\n    'firstName'?: string\n    'lastName'?: string\n}`,
+    it('checks the return types of read or write as a string', () => {
+        expect(uniqueDef.getTsTypeAsString()).toEqual({
+            read: `{\n    'firstName'?: string\n    'lastName'?: string\n}`,
+            write: `{\n    'firstName'?: string\n    'lastName'?: string\n}`,
+        });
+    });
+
+    it('accepts valid unique values', async () => {
+        const result = await uniqueDef.formatAndValidate({ firstName: 'test', lastName: 'man' });
+        expect(result).toEqual({ firstName: 'test', lastName: 'man' });
+    });
+
+    it('handles optional fields gracefully', async () => {
+        const result = await uniqueDef.formatAndValidate({ lastName: 'man' })
+        expect(result).toEqual({ lastName: 'man' })
     })
-  })
+})
 
-  it('accepts the value passed', async () => {
-    const result = await uniqueDef.formatAndValidate({ firstName: 'test', lastName: 'man' });
-    expect(result).toEqual({ firstName: 'test', lastName: 'man' });
-  })
 
-  //ATT: can't get the unique test to work
+//ATT: can't get the unique test to work
 //   it('throws an error if the unique value is not unique', () => {
 //     expect(uniqueDef.formatAndValidate({ firstName: 'test', lastName: 'test' }))
 //       .rejects
@@ -27,4 +36,3 @@ describe('Unique', () => {
 //         `Item should be unique. Another item with value: "test" for field "firstName" has been found`
 //       );
 //   })
-})
